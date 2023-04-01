144a3c38aa69a2a039e1a36dd0fce1ba82001190
ramsatti <ramsatti22@gmail.com>
2023-03-31 21:43:09 -0700

    My Commit

--- SequenceProgram2Variation.py
+++ SequenceProgram2Variation.py
@@ -1,11 +1,9 @@
-# input: startinNum = 0, delta = 2, n = 4
-# output: 0,2,4,6
-startinNum = 0  # int(input('What number do you want to start with? '))
-delta = 2  # int(input('What is the difference? '))
-n = 4  # (input('What is the ending number? '))
+startinNum = 0
+delta = 2
+n = 4
 
 for i in range(n):
-    print ('Index #', str(i), '= |', str(startinNum), '|')
-    int(i)
-    int(startinNum)
-    startinNum = startinNum + delta
+    print('#', i + 1, ':', startinNum, end='')
+    if i < n - 1:
+        print(', ', end=' ')
+    startinNum += delta
