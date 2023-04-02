d6b9c950764a60c65d968e17e04070a1acae0233
ramsatti <ramsatti22@gmail.com>
2023-04-01 19:52:22 -0700

    My Commit

--- test.py
+++
@@ -1,9 +0,0 @@
-array = [3, 2, 1, 0]
-anotherArray = []
-
-print('Before Forloop: anotherArray = ', anotherArray)
-for i in range(0, len(array)):
-    print('In ForLoop: i = ', i)
-    anotherArray.append(array[i-1])
-print('Outside Forloop: anotherArray = ', anotherArray)
-print(anotherArray)
