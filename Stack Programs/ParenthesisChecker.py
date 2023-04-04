ba06b8473de12255dc52ce33a5f3d40977961708
ramsatti <ramsatti22@gmail.com>
2023-04-03 19:11:40 -0700

    My Commit

---
+++ Stack Programs/ParenthesisChecker.py
@@ -0,0 +1,18 @@
+def parenthesisCheck(s: str) -> bool:
+    stack = []
+    closeToOpen = {')': '(', ']': '[', '}': '{'}
+           
+    for i in s:
+        if i in closeToOpen:
+            if stack and stack[-1] == closeToOpen[i]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(i)
+
+    return not stack
+
+str = input('Enter String w/ Parenthesis: ')
+isGood = parenthesisCheck(str)
+print(isGood)
