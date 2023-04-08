4a13a69037755633638284e83092de9de7459dea
ramsatti <ramsatti22@gmail.com>
2023-04-07 18:06:36 -0700

    My Commit

---
+++ Source Code Programs/AgeCalculator2023.py
@@ -0,0 +1,5 @@
+def ageCalc2023(year, month, date):
+    year = input('Type Year Born: '), 
+    month = input('Type Month Born: ')
+    date = input('Type Date Born: ')
+
--- Source Code Programs/PasAuthenticator.py
+++ Source Code Programs/PasAuthenticator.py
@@ -1,17 +1,19 @@
 import getpass
 
+print('Register Your Details')
 usernme = input('Username: ')
 paswrd = getpass.getpass('Password: ')
 print('You have been registered')
 
-def register(username, password):
+def verify(username, password):
     if username == usernme and password == paswrd:
         return 'You are logged in!'
     else:
         return 'Incorrect username or password'
 
+print('Login w/ Your Details')
 userIpt = input('Username: ')
 pwdIpt = getpass.getpass('Password: ')
 
-ans = register(userIpt, pwdIpt)
+ans = verify(userIpt, pwdIpt)
 print(ans)
